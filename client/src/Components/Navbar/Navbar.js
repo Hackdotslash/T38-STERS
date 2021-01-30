cb251c1514c759fd12cc54392178d6978c25e3cc
ridhdhish <ridhdhishdesai292000@gmail.com>
2021-01-30 23:54:45 +0530

    UI

---
+++ client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import classes from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMedkit,
+  faHome,
+  faClinicMedical,
+} from "@fortawesome/free-solid-svg-icons";
+
+const navbar = () => {
+  return (
+    <ul className={classes.Nav}>
+      <li>
+        <NavLink
+          activeStyle={{
+            fontWeight: "bold",
+            color: "red",
+          }}
+          to="/"
+        >
+          <span style={{ color: "#BCAAB2", fontSize: 24 }}>
+            <FontAwesomeIcon icon={faHome} />
+          </span>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName={classes.active} to="/camps">
+          <span style={{ color: "#BCAAB2", fontSize: 24 }}>
+            <FontAwesomeIcon icon={faMedkit} />
+          </span>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName={classes.active} to="/hospitals">
+          <span style={{ color: "#BCAAB2", fontSize: 24 }}>
+            <FontAwesomeIcon icon={faClinicMedical} />
+          </span>
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default navbar;
