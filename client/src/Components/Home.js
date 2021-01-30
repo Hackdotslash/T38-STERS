04d5824fc313653ff68298d574a0244fbd3a67ff
ridhdhish <ridhdhishdesai292000@gmail.com>
2021-01-30 23:49:04 +0530

    UI

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "./Navbar/Navbar";
 
 const Home = (props) => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      <h1 style={{ marginLeft: 150 }}>Home</h1>
+    </div>
+  );
 };
 
 export default Home;
